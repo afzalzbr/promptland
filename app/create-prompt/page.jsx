cf1d4c1dcf53ee2f37f0564bad254f0759f9f796
A. Zubair <afzal.zubair@khi.iba.edu.pk>
2023-08-31 20:52:00 +0100

    create post/prompt done

--- app/create-prompt/page.jsx
+++ app/create-prompt/page.jsx
@@ -1,9 +1,55 @@
-import React from 'react'
+"use client";
+
+import { useState } from "react";
+import { useSession } from "next-auth/react";
+import { useRouter } from "next/navigation";
+
+import Form from "@components/Form";
 
 const CreatePrompt = () => {
-  return (
-    <div>CreatePrompt</div>
-  )
-}
+  const router = useRouter();
+  const { data: session } = useSession();
+  const [submitting, setSubmitting] = useState(false);
+  const [post, setPost] = useState({ prompt: "", tag: "" });
 
-export default CreatePrompt
+  const createPrompt = async (e) => {
+    e.preventDefault();
+    setSubmitting(true);
+    try {
+      const res = await fetch("/api/prompt/new", {
+        method: "POST",
+        // headers: {
+        //   "Content-Type": "application/json",
+        // },
+        body: JSON.stringify({
+          prompt: post.prompt,
+          userId: session?.user.id,
+          tag: post.tag,
+        }),
+      });
+
+      if (res.ok) {
+        router.push("/");
+      }
+      // setSubmitting(false);
+      // const json = await res.json();
+      // if (!res.ok) throw Error(json.message);
+      // alert("Prompt created!");
+      // setPost({ prompt: "", tag: "" });
+    } catch (e) {
+      throw Error(e.message);
+    }
+  };
+
+  return (
+    <Form
+      type="Create"
+      post={post}
+      setPost={setPost}
+      submitting={submitting}
+      onSubmit={createPrompt}
+    />
+  );
+};
+
+export default CreatePrompt;
