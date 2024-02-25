36513bab79aaf15c8e296cfa6a9e6a94e435e6a1
Afzal Zubair <afzal.zubair@khi.iba.edu.pk>
2024-02-25 00:23:34 +0000

    Update layout.jsx

--- app/layout.jsx
+++ app/layout.jsx
@@ -3,6 +3,7 @@ import "@styles/globals.css";
 import Nav from "@components/Nav";
 import Provider from "@components/Provider";
 import Script from "next/script";
+import Head from "next/head";
 
 export const metadata = {
   title: "Promptland",
@@ -12,6 +13,21 @@ export const metadata = {
 const RootLayout = ({ children }) => {
   return (
     <html lang="en">
+      {/* <Head>
+        <script
+          type="text/javascript"
+          dangerouslySetInnerHTML={{
+            __html: `
+          <script>
+              window.__lc = window.__lc || {};
+              window.__lc.license = 17248392;
+              ;(function(n,t,c){function i(n){return e._h?e._h.apply(null,n):e._q.push(n)}var e={_q:[],_h:null,_v:"2.0",on:function(){i(["on",c.call(arguments)])},once:function(){i(["once",c.call(arguments)])},off:function(){i(["off",c.call(arguments)])},get:function(){if(!e._h)throw new Error("[LiveChatWidget] You can't use getters before load.");return i(["get",c.call(arguments)])},call:function(){i(["call",c.call(arguments)])},init:function(){var n=t.createElement("script");n.async=!0,n.type="text/javascript",n.src="https://cdn.livechatinc.com/tracking.js",t.head.appendChild(n)}};!n.__lc.asyncInit&&e.init(),n.LiveChatWidget=n.LiveChatWidget||e}(window,document,[].slice))
+          </script>
+          <noscript><a href="https://www.livechat.com/chat-with/17248392/" rel="nofollow">Chat with us</a>, powered by <a href="https://www.livechat.com/?welcome" rel="noopener nofollow" target="_blank">LiveChat</a></noscript>
+            `,
+          }}
+        />
+      </Head> */}
       <body>
         <Provider>
           <div className="main">
@@ -26,14 +42,12 @@ const RootLayout = ({ children }) => {
       </body>
       <Script id="live-chat-w" strategy="lazyOnload">
         {`
-          <!-- Start of LiveChat (www.livechat.com) code -->
           <script>
               window.__lc = window.__lc || {};
               window.__lc.license = 17248392;
               ;(function(n,t,c){function i(n){return e._h?e._h.apply(null,n):e._q.push(n)}var e={_q:[],_h:null,_v:"2.0",on:function(){i(["on",c.call(arguments)])},once:function(){i(["once",c.call(arguments)])},off:function(){i(["off",c.call(arguments)])},get:function(){if(!e._h)throw new Error("[LiveChatWidget] You can't use getters before load.");return i(["get",c.call(arguments)])},call:function(){i(["call",c.call(arguments)])},init:function(){var n=t.createElement("script");n.async=!0,n.type="text/javascript",n.src="https://cdn.livechatinc.com/tracking.js",t.head.appendChild(n)}};!n.__lc.asyncInit&&e.init(),n.LiveChatWidget=n.LiveChatWidget||e}(window,document,[].slice))
           </script>
           <noscript><a href="https://www.livechat.com/chat-with/17248392/" rel="nofollow">Chat with us</a>, powered by <a href="https://www.livechat.com/?welcome" rel="noopener nofollow" target="_blank">LiveChat</a></noscript>
-          <!-- End of LiveChat code -->
           
         `}
       </Script>
